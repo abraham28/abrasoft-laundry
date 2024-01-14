1bd6d55066f9c1a80a6231ae6665f47da975f31e
abratestenv <abrahampoog@gmail.com>
2024-01-14 19:21:12 +0800

    fixed constants file due to static baseurl

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -38,41 +38,15 @@ export const authOptions: NextAuthOptions = {
             throw new Error(errorData.error);
           }
         } catch (error) {
-          return null;
-          // if (error instanceof Error) {
-          //   throw new Error(error.message);
-          // } else {
-          //   throw new Error("Unknown error occured");
-          // }
+          if (error instanceof Error) {
+            throw new Error(error.message);
+          } else {
+            throw new Error("Unknown error occured");
+          }
         }
       },
     }),
   ],
-  callbacks: {
-    async jwt({ user, token }) {
-      if (user) token.sub = user.id;
-      return token;
-    },
-    async session({
-      session,
-      //  token
-    }) {
-      if (session && session.user) {
-        // if (token.sub) {
-        //   session.user["id"] = token.sub;
-        // }
-        // const res = await fetch(
-        //   `https://lc-ranklist.vercel.app/api/getUsernameStatus?user_id=${session.user.id}`,
-        // );
-        // if (res.status === 409) {
-        //   const response = await res.json();
-        //   session.user.lc_username = response.data[0].lc_username;
-        //   session.user.org = response.data[0].org;
-        // }
-      }
-      return session;
-    },
-  },
   secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
   pages: {
